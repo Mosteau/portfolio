7f026aed6b21fe2f1c2917c673d18a5f4dafe99f
Mosteau <thibaut.mosteau@lilo.org>
2024-02-14 12:06:58 +0100

    third commit

--- src/App.tsx
+++ src/App.tsx
@@ -25,9 +25,12 @@ function App() {
     >
       <div
         id="page-global"
-        className="bg-custom-black  min-h-screen text-center pt-5"
+        className="bg-custom-black  min-h-screen text-center pt-6"
       >
-        <div id="navbar" className="flex flex-wrap justify-around items-center mx-auto opacity-90">
+        <div
+          id="navbar"
+          className="flex flex-wrap justify-around items-center mx-auto opacity-90"
+        >
           <a
             href="/CV2024.pdf"
             className="font-dosis text-custom-white text-2xl hover:text-red-500 transform hover:scale-110 transition-transform"
@@ -65,7 +68,10 @@ function App() {
             {" FullStack developer!"}
           </h1>
         </div>
-        <div id="stack presentation" className="container mx-auto py-9 md:py-12 px-4 md:px-6 opacity-90">
+        <div
+          id="stack presentation"
+          className="container mx-auto py-9 md:py-12 px-4 md:px-6 opacity-90"
+        >
           <div
             data-aos="fade-up"
             className="flex items-strech justify-center flex-col space-y-4 md:space-y-0 md:space-x-6 lg:space-x-8 mt-20"
@@ -82,7 +88,7 @@ function App() {
           </div>
           <div
             data-aos="fade-up"
-            className="flex flex-wrap justify-between items-center mx-auto p-4 w-4/5"
+            className="flex flex-wrap justify-between items-center mx-auto pt-8 p-4 w-4/5"
           >
             <img
               src="./images/logos/linux.png"
@@ -169,207 +175,210 @@ function App() {
         <section id="projects" className="py-16">
           <div data-aos="fade-up" className="container mx-auto px-4 mt-20">
             <h2 className="text-5xl font-dosis mb-20 text-center text-custom-white opacity-90">
-              🚀 Projects
+              🚀 <p className="p-5">Projects</p>
             </h2>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
-              <div
-                data-aos="fade-up"
-                className="bg-neutral-300 rounded-2xl overflow-hidden m-4 flex flex-col justify-center items-center"
-                style={{ boxShadow: "0 2px 2px rgba(255, 255, 255, 0.5)" }}
-              >
-                <a
-                  href="http://vps-d5babf40.vps.ovh.net:5002/"
-                  target="_blank"
-                  className="block text-black-600 hover:underline mt-4"
+              <div className="hover:scale-105 transition-all">
+                <div
+                  data-aos="fade-up"
+                  className="bg-neutral-300 rounded-2xl overflow-hidden m-4 flex flex-col justify-center items-center "
                 >
-                  <img
-                    src="./images/projets/hubideablack.png"
-                    alt="Project Image"
-                    className="object-contain h-40 w-3/4 inline-block"
-                  ></img>
-                  <div className="pt-4 pb-4">
-                    <h3 className="text-2xl font-dosis mb-4 opacity-90">
-                      Hub Idea
-                    </h3>
-                    <div className="flex justify-center flex-wrap">
-                      <img
-                        src="./images/logos/iconscard/jscard.png"
-                        alt="javascript"
-                        title="javascript"
-                        className="p-2 object-contain opacity-80"
-                      />
-                      <img
-                        src="./images/logos/iconscard/reactcard.png"
-                        alt="reacts"
-                        title="react"
-                        className="p-2 object-contain opacity-80"
-                      />
-                      <img
-                        src="./images/logos/iconscard/nodejs.png"
-                        alt="node"
-                        title="node"
-                        className="p-2 object-contain opacity-80"
-                      />
-                      <img
-                        src="./images/logos/iconscard/expresscard.png"
-                        alt="express"
-                        title="express"
-                        className="p-2 object-contain opacity-80"
-                      />
-                      <img
-                        src="./images/logos/iconscard/sasscard.png"
-                        alt="sass"
-                        title="sass"
-                        className="p-2 object-contain opacity-80"
-                      />
-                      <img
-                        src="./images/logos/iconscard/mysql.png"
-                        alt="mysql"
-                        title="mysql"
-                        className="p-2 object-contain opacity-80"
-                      />
+                  <a
+                    href="http://vps-d5babf40.vps.ovh.net:5002/"
+                    target="_blank"
+                    className="block text-black-600 mt-4 "
+                  >
+                    <img
+                      src="./images/projets/hubideablack.png"
+                      alt="Project Image"
+                      className="object-contain h-40 w-3/4 inline-block"
+                    ></img>
+                    <div className="pt-4 pb-4">
+                      <h3 className="text-2xl font-dosis mb-4 opacity-90">
+                        Hub Idea
+                      </h3>
+                      <div className="flex justify-center flex-wrap">
+                        <img
+                          src="./images/logos/iconscard/jscard.png"
+                          alt="javascript"
+                          title="javascript"
+                          className="p-2 object-contain opacity-80"
+                        />
+                        <img
+                          src="./images/logos/iconscard/reactcard.png"
+                          alt="reacts"
+                          title="react"
+                          className="p-2 object-contain opacity-80"
+                        />
+                        <img
+                          src="./images/logos/iconscard/nodejs.png"
+                          alt="node"
+                          title="node"
+                          className="p-2 object-contain opacity-80"
+                        />
+                        <img
+                          src="./images/logos/iconscard/expresscard.png"
+                          alt="express"
+                          title="express"
+                          className="p-2 object-contain opacity-80"
+                        />
+                        <img
+                          src="./images/logos/iconscard/sasscard.png"
+                          alt="sass"
+                          title="sass"
+                          className="p-2 object-contain opacity-80"
+                        />
+                        <img
+                          src="./images/logos/iconscard/mysql.png"
+                          alt="mysql"
+                          title="mysql"
+                          className="p-2 object-contain opacity-80"
+                        />
+                      </div>
                     </div>
-                  </div>
-                </a>
+                  </a>
+                </div>
               </div>
-
-              <div
-                data-aos="fade-up"
-                className="bg-neutral-300 rounded-2xl overflow-hidden m-4 flex flex-col justify-center items-center"
-                style={{ boxShadow: "0 2px 2px rgba(255, 255, 255, 0.5)" }}
-              >
-                <a
-                  href="https://radioworld.vercel.app/"
-                  target="_blank"
-                  className="block text-black-600 hover:underline mt-4"
+              <div className="hover:scale-105 transition-all">
+                <div
+                  data-aos="fade-up"
+                  className="bg-neutral-300 rounded-2xl overflow-hidden m-4 flex flex-col justify-center items-center"
                 >
-                  <img
-                    src="./images/projets/radioworld2.png"
-                    alt="Project Image"
-                    className="object-contain h-40 w-3/4 inline-block"
-                  ></img>
-                  <div className="pt-4 pb-4">
-                    <h3 className="text-2xl font-dosis  mb-4 opacity-90">
-                      Radio World
-                    </h3>
-                    <div className="flex justify-center flex-wrap">
-                      <img
-                        src="./images/logos/iconscard/jscard.png"
-                        alt="javascript"
-                        title="javascript"
-                        className="p-2 object-contain opacity-80"
-                      />
-                      <img
-                        src="./images/logos/iconscard/reactcard.png"
-                        alt="reacts"
-                        title="react"
-                        className="p-2 object-contain opacity-80"
-                      />
-                      <img
-                        src="./images/logos/iconscard/nodejs.png"
-                        alt="node"
-                        title="node"
-                        className="p-2 object-contain opacity-80"
-                      />
-                      <img
-                        src="./images/logos/iconscard/expresscard.png"
-                        alt="express"
-                        title="express"
-                        className="p-2 object-contain opacity-80"
-                      />
-                      <img
-                        src="./images/logos/iconscard/sasscard.png"
-                        alt="sass"
-                        title="sass"
-                        className="p-2 object-contain opacity-80"
-                      />
+                  <a
+                    href="https://radioworld.vercel.app/"
+                    target="_blank"
+                    className="block text-black-600 mt-4"
+                  >
+                    <img
+                      src="./images/projets/radioworld2.png"
+                      alt="Project Image"
+                      className="object-contain h-40 w-3/4 inline-block"
+                    ></img>
+                    <div className="pt-4 pb-4">
+                      <h3 className="text-2xl font-dosis  mb-4 opacity-90">
+                        Radio World
+                      </h3>
+                      <div className="flex justify-center flex-wrap">
+                        <img
+                          src="./images/logos/iconscard/jscard.png"
+                          alt="javascript"
+                          title="javascript"
+                          className="p-2 object-contain opacity-80"
+                        />
+                        <img
+                          src="./images/logos/iconscard/reactcard.png"
+                          alt="reacts"
+                          title="react"
+                          className="p-2 object-contain opacity-80"
+                        />
+                        <img
+                          src="./images/logos/iconscard/nodejs.png"
+                          alt="node"
+                          title="node"
+                          className="p-2 object-contain opacity-80"
+                        />
+                        <img
+                          src="./images/logos/iconscard/expresscard.png"
+                          alt="express"
+                          title="express"
+                          className="p-2 object-contain opacity-80"
+                        />
+                        <img
+                          src="./images/logos/iconscard/sasscard.png"
+                          alt="sass"
+                          title="sass"
+                          className="p-2 object-contain opacity-80"
+                        />
+                      </div>
                     </div>
-                  </div>
-                </a>
+                  </a>
+                </div>
               </div>
-              <div
-                data-aos="fade-up"
-                className="bg-neutral-300 rounded-2xl overflow-hidden m-4 flex flex-col justify-center items-center"
-                style={{ boxShadow: "0 2px 2px rgba(255, 255, 255, 0.5)" }}
-              >
-                <a
-                  href="https://famille-inalti.vercel.app/"
-                  target="_blank"
-                  className="block text-black-600 hover:underline mt-4"
+              <div className="hover:scale-105 transition-all">
+                <div
+                  data-aos="fade-up"
+                  className="bg-neutral-300 rounded-2xl overflow-hidden m-4 flex flex-col justify-center items-center"
                 >
-                  <img
-                    src="./images/projets/FamilleInaltiblack.png"
-                    alt="Project Image"
-                    className="object-contain h-40 w-3/4 inline-block"
-                  ></img>
-                  <div className="pt-4 pb-4">
-                    <h3 className="text-2xl font-dosis  mb-4 opacity-90">
-                      Famille Inalty
-                    </h3>
-                    <div className="flex justify-center flex-wrap">
-                      <img
-                        src="./images/logos/iconscard/jscard.png"
-                        alt="javascript"
-                        title="javascript"
-                        className="p-2 object-contain opacity-80"
-                      />
-                      <img
-                        src="./images/logos/iconscard/reactcard.png"
-                        alt="reacts"
-                        title="react"
-                        className="p-2 object-contain opacity-80"
-                      />
-                      <img
-                        src="./images/logos/iconscard/sasscard.png"
-                        alt="sass"
-                        title="sass"
-                        className="p-2 object-contain opacity-80"
-                      />
+                  <a
+                    href="https://famille-inalti.vercel.app/"
+                    target="_blank"
+                    className="block text-black-600 mt-4"
+                  >
+                    <img
+                      src="./images/projets/FamilleInaltiblack.png"
+                      alt="Project Image"
+                      className="object-contain h-40 w-3/4 inline-block"
+                    ></img>
+                    <div className="pt-4 pb-4">
+                      <h3 className="text-2xl font-dosis  mb-4 opacity-90">
+                        Famille Inalty
+                      </h3>
+                      <div className="flex justify-center flex-wrap">
+                        <img
+                          src="./images/logos/iconscard/jscard.png"
+                          alt="javascript"
+                          title="javascript"
+                          className="p-2 object-contain opacity-80"
+                        />
+                        <img
+                          src="./images/logos/iconscard/reactcard.png"
+                          alt="reacts"
+                          title="react"
+                          className="p-2 object-contain opacity-80"
+                        />
+                        <img
+                          src="./images/logos/iconscard/sasscard.png"
+                          alt="sass"
+                          title="sass"
+                          className="p-2 object-contain opacity-80"
+                        />
+                      </div>
                     </div>
-                  </div>
-                </a>
+                  </a>
+                </div>
               </div>
-              <div
-                data-aos="fade-up"
-                className="bg-neutral-300 rounded-2xl overflow-hidden m-4 flex flex-col justify-center items-center"
-                style={{ boxShadow: "0 2px 2px rgba(255, 255, 255, 0.5)" }}
-              >
-                <a
-                  href="https://spookie-clicker.vercel.app/"
-                  target="_blank"
-                  className="block text-black-600 hover:underline mt-4"
+              <div className="hover:scale-105 transition-all">
+                <div
+                  data-aos="fade-up"
+                  className="bg-neutral-300 rounded-2xl overflow-hidden m-4 flex flex-col justify-center items-center"
                 >
-                  <img
-                    src="./images/projets/sp-game.png"
-                    alt="Project Image"
-                    className="object-contain h-40 w-3/4 inline-block"
-                  ></img>
-                  <div className="pt-4 pb-4">
-                    <h3 className="text-2xl font-dosis mb-4 opacity-90">
-                      Spookie Clicker
-                    </h3>
-                    <div className="flex justify-center flex-wrap">
-                      <img
-                        src="./images/logos/iconscard/jscard.png"
-                        alt="javascript"
-                        title="javascript"
-                        className="p-2 object-contain opacity-80"
-                      />
-                      <img
-                        src="./images/logos/iconscard/csscard.png"
-                        alt="css"
-                        title="css"
-                        className="p-2 object-contain opacity-80"
-                      />
-                      <img
-                        src="./images/logos/iconscard/reactcard.png"
-                        alt="reacts"
-                        title="react"
-                        className="p-2 object-contain opacity-80"
-                      />
+                  <a
+                    href="https://spookie-clicker.vercel.app/"
+                    target="_blank"
+                    className="block text-black-600 mt-4"
+                  >
+                    <img
+                      src="./images/projets/sp-game.png"
+                      alt="Project Image"
+                      className="object-contain h-40 w-3/4 inline-block"
+                    ></img>
+                    <div className="pt-4 pb-4">
+                      <h3 className="text-2xl font-dosis mb-4 opacity-90">
+                        Spookie Clicker
+                      </h3>
+                      <div className="flex justify-center flex-wrap">
+                        <img
+                          src="./images/logos/iconscard/jscard.png"
+                          alt="javascript"
+                          title="javascript"
+                          className="p-2 object-contain opacity-80"
+                        />
+                        <img
+                          src="./images/logos/iconscard/csscard.png"
+                          alt="css"
+                          title="css"
+                          className="p-2 object-contain opacity-80"
+                        />
+                        <img
+                          src="./images/logos/iconscard/reactcard.png"
+                          alt="reacts"
+                          title="react"
+                          className="p-2 object-contain opacity-80"
+                        />
+                      </div>
                     </div>
-                  </div>
-                </a>
+                  </a>
+                </div>
               </div>
             </div>
           </div>
